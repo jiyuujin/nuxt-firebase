2ce6b41f61094baab75cd30636ec176ba81fb954
jiyuujin <jiyuujinunite@gmail.com>
2023-04-28 06:19:13 +0900

    nuxt v3 stable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
+import svgLoader from 'vite-svg-loader'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
@@ -8,5 +8,11 @@ export default defineNuxtConfig({
   serverMiddleware: [
     { path: '/api/hello', handler: '~/server/api/hello.ts'},
   ],
-  buildModules: ['@nuxtjs/svg']
+  vite: {
+    plugins: [
+      svgLoader({
+        svgo: false,
+      }),
+    ],
+  },
 })
